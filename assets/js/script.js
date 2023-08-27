bf22894daed31d78978998aea87aab766fd237ae
PRich57 <pcrichards57@gmail.com>
2023-08-27 14:11:10 -0600

    Pseudocode js and build starter code of html

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// VARIABLE DECLARATIONS
+
+// Declare timer variable with starting time
+
+
+
+// FUNCTIONS
+// Create time interval function
+
+// While time != 0 && value at array variable != undefined
+
+// Add multiple choice question and answers to screen
+
+// Internal function upon button click event listener
+
+  // Add hr between last multiple choice option and display of Correct or Wrong
+
+  // If !choice, decrement timer variable by 5
+
+  // Display Wrong beneath hr
+
+  // Else
+
+  // Display Correct beneath hr
+
+// Increment multipleChoice array for next question and possible answers
+
+
+// EVENT LISTENERS
