dcfb1dcffdac1fd98e971c825bfd19cb66f446d9
PRich57 <pcrichards57@gmail.com>
2023-08-28 09:16:42 -0600

    Add buttons via js for multiple choice answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,8 +17,14 @@ var li1 = document.createElement("li");
 var li2 = document.createElement("li");
 var li3 = document.createElement("li");
 var li4 = document.createElement("li");
+// Create button elements for each li
+var button1 = document.createElement("button");
+var button2 = document.createElement("button");
+var button3 = document.createElement("button");
+var button4 = document.createElement("button");
+// Create 
 
-// Append timer to the
+// APPEND ELEMENTS
 header.appendChild(timerEl);
 main.appendChild(h2El);
 main.appendChild(listEl);
@@ -26,6 +32,10 @@ listEl.appendChild(li1);
 listEl.appendChild(li2);
 listEl.appendChild(li3);
 listEl.appendChild(li4);
+li1.appendChild(button1);
+li2.appendChild(button2);
+li3.appendChild(button3);
+li4.appendChild(button4);
 
 // Create object array with each object containing questions, answers, and correctAnswer properties
 var quiz = [
@@ -101,35 +111,36 @@ function countdown() {
 
 
 h2El.textContent = quiz[0].question;
-li1.textContent = quiz[0].answers.a;
-li2.textContent = quiz[0].answers.b;
-li3.textContent = quiz[0].answers.c;
-li4.textContent = quiz[0].answers.d;
+button1.textContent = quiz[0].answers.a;
+button2.textContent = quiz[0].answers.b;
+button3.textContent = quiz[0].answers.c;
+button4.textContent = quiz[0].answers.d;
 console.log(h2El);
 console.log(li1);
 console.log(li2);
 console.log(li3);
 console.log(li4);
-// FIGURE THIS OUT
-// function myQuiz() {
-//   
+
+
+// FIGURE THIS OUT - On button click, if start button, pull up quiz[0], else if quiz[i] === undefined, pull up all done page, else, pull up next question
+function myQuiz() {
+  
 //   // For each iteration, add next question and possible answers
 
-//   for (var i = 0; i < quiz.length; i++) {
-//     if (quiz[i] !== undefined) {
-  //      addEventListener here
-  //     // Clear out main content
-  //     main.textContent = " ";
-  //  
-         // Add value of each corresponding key in the quiz object array
-  //     h2El.textContent = quiz[i].question;
-  //     li1.textContent = quiz[i].answers.a;
-  //     li2.textContent = quiz[i].answers.b;
-  //     li3.textContent = quiz[i].answers.c;
-  //     li4.textContent = quiz[i].answers.d;
-  //   }
-//   }
-// }
+  for (var i = 0; i < quiz.length; i++) {
+    if (quiz[i] !== undefined) {
+      //  addEventListener here for click on button class choice
+      // Clear out main content
+      main.textContent = " ";
+      // Add value of each corresponding key in the quiz object array
+      h2El.textContent = quiz[i].question;
+      li1.textContent = quiz[i].answers.a;
+      li2.textContent = quiz[i].answers.b;
+      li3.textContent = quiz[i].answers.c;
+      li4.textContent = quiz[i].answers.d;
+    }
+  }
+}
 // myQuiz();
 
 // While time != 0 && value at array variable != undefined
