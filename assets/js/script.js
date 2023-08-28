d523f96fe629524e15f9f78163484b35b06abfda
PRich57 <pcrichards57@gmail.com>
2023-08-27 18:23:10 -0600

    Work on myQuiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,10 @@
 
 // Create html elements and store them in variables
 var body = document.body;
+// Declare header variable
 var header = document.getElementById("header");
-console.log(header);
+// Declare main variable
+var main = document.getElementById("main");
 // timerEl will be the timer element
 var timerEl = document.createElement("p");
 // h2El will hold the current question
@@ -16,8 +18,14 @@ var li2 = document.createElement("li");
 var li3 = document.createElement("li");
 var li4 = document.createElement("li");
 
-// Append start page to the DOM
+// Append timer to the
 header.appendChild(timerEl);
+main.appendChild(h2El);
+main.appendChild(listEl);
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
 
 // Create object array with each object containing questions, answers, and correctAnswer properties
 var quiz = [
@@ -73,32 +81,49 @@ var quiz = [
   },
 ];
 
-
-
 // FUNCTIONS
 // Create countdown function
 function countdown() {
   timeLeft = 75;
 
-  var timeInterval = setInterval(function() {
+  var timeInterval = setInterval(function () {
     timeLeft--;
     timerEl.textContent = "Time: " + timeLeft;
-    
+
     // Exit condition
     if (timeLeft === 0) {
       displayScore();
       clearInterval(timeInterval);
+      return;
     }
   }, 1000);
 }
 
-function displayScore() {
-  var score = timeLeft;
+
+
+// FIGURE THIS OUT
+function myQuiz() {
+  var questionNumber = 1;
+  // For each iteration, add next question and possible answers
+  questionNumber++;
+
+  // while quiz[index] is not undefined
+  while (quiz[questionNumber] !== undefined) {
+    // Clear out main content
+    main.textContent = " ";
+    // Add value of each corresponding key in the quiz object array
+    h2El.textContent = quiz[questionNumber].question;
+    li1.textContent = quiz[questionNumber].answers.a;
+    li2.textContent = quiz[questionNumber].answers.b;
+    li3.textContent = quiz[questionNumber].answers.c;
+    li4.textContent = quiz[questionNumber].answers.d;
+  }
 }
+myQuiz();
+
 // While time != 0 && value at array variable != undefined
 
 // Add multiple choice question and answers to screen
-
 // Internal function upon button click event listener
 
 // Add hr between last multiple choice option and display of Correct or Wrong
