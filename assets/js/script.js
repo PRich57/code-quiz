315a588887fbcd61c933a42f51c0ecc4fa0b90cd
PRich57 <pcrichards57@gmail.com>
2023-08-28 17:27:37 -0600

    Display first question on start button click and display next on choice click

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ var body = document.body;
 var header = document.getElementById("header");
 // Declare main variable
 var main = document.getElementById("main");
+// Declare start section variable
+var sContainer = document.querySelector("#start");
 // timerEl will be the timer element
 var timerEl = document.createElement("p");
 // h2El will hold the current question
@@ -22,23 +24,16 @@ var button1 = document.createElement("button");
 var button2 = document.createElement("button");
 var button3 = document.createElement("button");
 var button4 = document.createElement("button");
-// Create
+// Create variable as reference to start button
+var start = document.querySelector("#start-button");
+// Set index to 0
+var index = 0;
 
 // APPEND ELEMENTS
 header.appendChild(timerEl);
 main.appendChild(h2El);
-main.appendChild(listEl);
-listEl.appendChild(li1);
-listEl.appendChild(li2);
-listEl.appendChild(li3);
-listEl.appendChild(li4);
-li1.appendChild(button1);
-li2.appendChild(button2);
-li3.appendChild(button3);
-li4.appendChild(button4);
 
 // Create object array with each object containing questions, answers, and correctAnswer properties
-// Consider making choices an array instead of an object
 var quiz = [
   {
     question: "Question 1",
@@ -77,11 +72,80 @@ var quiz = [
 // show quiz section
 // display a question
 
+start.addEventListener("click", function (event) {
+  // Stop from bubbling up
+  event.stopPropagation();
+
+  // Clear content
+  sContainer.textContent = " ";
+
+  // Declare element variable to target the section to show
+  var element = document.querySelector("#quiz");
+
+  // Show quiz section
+  if (element.matches("section")) {
+    // get current state
+    var state = element.getAttribute("data-state");
+
+    if (state === "hidden") {
+      // Update data-state to hidden
+      element.dataset.state = "visible";
+
+      // element.textContent = quiz[0];
+      h2El.textContent = quiz[0].question;
+      button1.textContent = quiz[0].choices[0];
+      button2.textContent = quiz[0].choices[1];
+      button3.textContent = quiz[0].choices[2];
+      button4.textContent = quiz[0].choices[3];
+
+      // Append content
+      main.appendChild(listEl);
+      listEl.appendChild(li1);
+      listEl.appendChild(li2);
+      listEl.appendChild(li3);
+      listEl.appendChild(li4);
+      li1.appendChild(button1);
+      li2.appendChild(button2);
+      li3.appendChild(button3);
+      li4.appendChild(button4);
+    }
+  }
+});
+
+listEl.addEventListener("click", function(event) {
+  event.stopPropagation();
+  displayQuestion();
+});
+
+function displayQuestion() {
+  index++;
+
+  if (index > quiz.length - 1) {
+    displayScore();
+  } else {
+    var check = checkAnswer();
+    if (check){
+      h2El.textContent = quiz[index].question;
+      button1.textContent = quiz[index].choices[0];
+      button2.textContent = quiz[index].choices[1];
+      button3.textContent = quiz[index].choices[2];
+      button4.textContent = quiz[index].choices[3];
+      var hr = document.createElement("hr");
+      main.appendChild(hr);
+      var correct = document.createElement("h3");
+      correct.textContent = "Correct";
+      main.appendChild(correct);
+    } else {
+      
+    }
+  }
+}
+
 // quiz section
 // array of objects
-// question
-// array of answers
-// correct answer
+  // question
+  // array of answers
+  // correct answer
 // could list the index of the correct answer or put the string of the correct answer
 // Build a displayQuestion()
 // Attach click event to each option
@@ -116,24 +180,18 @@ function countdown() {
 
     // Exit condition
     if (timeLeft === 0) {
-      displayScore();
+      // displayScore(); - need to create this function
       clearInterval(timeInterval);
       return;
     }
   }, 1000);
 }
 
-h2El.textContent = quiz[0].question;
-button1.textContent = quiz[0].choices[0];
-button2.textContent = quiz[0].choices[1];
-button3.textContent = quiz[0].choices[2];
-button4.textContent = quiz[0].choices[3];
-
 // FIGURE THIS OUT - On button click, if start button, pull up quiz[0], else if quiz[i] === undefined, pull up all done page, else, pull up next question
 // Look at the class activities 15 to help with the event listener setup. This might need to be an event listener function from the start.
-// Turn this into a carosel where each question exists in html and JS shows or hides the questions on click
-function myQuiz() {
-  //   // For each iteration, add next question and possible answers
+// Turn this into a carousel where JS shows or hides the questions on click
+function displayQuiz() {
+  // For each iteration, add next question and possible answers
 
   for (var i = 0; i < quiz.length; i++) {
     if (quiz[i] !== undefined) {
