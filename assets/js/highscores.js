00ba823b75f1f36d33a4232deb8d498dd8b1eb35
PRich57 <pcrichards57@gmail.com>
2023-08-29 22:43:40 -0600

    Create highscore.js code to display highscores in descending order

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,18 +1,21 @@
 var highScorePage = document.querySelector("#highScores");
 var highScoreCountSpan = document.querySelector("#scoreCount");
-
-console.log(highScores);
+var highScores = JSON.parse(localStorage.getItem("high-scores"));
+var scores = [];
 
 // Create function to render high scores
 function renderScores() {
   highScorePage.textContent = "";
   highScoreCountSpan.textContent = highScores.length;
-
   for (var i = 0; i < highScores.length; i++) {
-    var highScore = highScores[i];
+    // Sort by descending order function
+    highScores.sort(descendingOrder);
+
+    var initials = highScores[i].initials;
+    var highScore = highScores[i].score;
 
     var li = document.createElement("li");
-    li.textContent = highScore;
+    li.textContent = initials + " - " + highScore;
     li.setAttribute("data-index", i);
 
     highScorePage.appendChild(li);
@@ -20,3 +23,13 @@ function renderScores() {
 
 }
 renderScores();
+
+function descendingOrder(a, b) {
+  if (a.score < b.score) {
+    return 1;
+  }
+  if (a.score > b.score) {
+    return -1;
+  }
+  return 0;
+}
