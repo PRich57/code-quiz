9d0a8cba23630aa4843b89b7f9eabe0706ea2657
PRich57 <pcrichards57@gmail.com>
2023-08-30 23:22:14 -0600

    Finish comments in highscores.js

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,3 +1,4 @@
+// VARIABLE DECLARATIONS
 var highScorePage = document.querySelector("#highScoresList");
 var footer = document.querySelector("footer");
 var h1El = document.querySelector("#h1El");
@@ -5,31 +6,38 @@ var clearBtn = document.createElement("button");
 clearBtn.textContent = "Clear All Scores";
 footer.appendChild(clearBtn);
 
+// FUNCTIONS
 // Create function to render high scores
 function renderScores() {
   h1El.textContent = "High Scores";
+  // Get local storage and store in variable as array of objects
   var highScores = JSON.parse(localStorage.getItem("high-scores"));
+  // Exit condition
   if (highScores === null) {
     return;
   }
+  // Loop through the array of objects
   for (var i = 0; i < highScores.length; i++) {
     // Sort by descending order function
     highScores.sort(descendingOrder);
 
+    // Create variable to store current index set of initials in uppercase
     var initials = highScores[i].initials.toUpperCase();
+    // Create variable to store current index score
     var highScore = highScores[i].score;
 
+    // Create variable to store reference to newly created li element
     var li = document.createElement("li");
+    // Add text content of current index initials and score to li
     li.textContent = initials + " - " + highScore;
-    li.setAttribute("data-index", i);
+    // Set attribute for css reference
     li.setAttribute("class", "highScoresItems");
-
+    // Append new li
     highScorePage.appendChild(li);
   }
 }
 
-renderScores();
-
+// Create function to sort scores by descending order
 function descendingOrder(a, b) {
   if (a.score < b.score) {
     return 1;
@@ -40,12 +48,19 @@ function descendingOrder(a, b) {
   return 0;
 }
 
+// Create function to clear scores from memory and DOM
 function clearScores() {
   localStorage.removeItem("high-scores");
+  highScorePage.textContent = "";
 }
 
+// EVENT LISTENERS
+// Listen for click on clearBtn and call clearScores()
 clearBtn.addEventListener("click", function (event) {
   event.stopPropagation();
   clearScores();
-  highScorePage.textContent = "";
 });
+
+// FUNCTION CALLS
+// Call renderScores()
+renderScores();
